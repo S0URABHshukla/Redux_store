0e78777886ee0ce87b78cf8fcbf0d8055c1a518d
Sourabh Shukla <sourabh01itsme@gmail.com>
2023-01-27 16:00:10 +0530

    javascripts qus ans

---
+++ Java scripts/javascript.js
@@ -0,0 +1,56 @@
+// let animals=[{type:"lion"},"tiger"];
+// let clones=animals.slice();
+
+// clones[0].type="bear";
+// clones[1]="sheep";
+// console.log(animals[0].type, clones[0].typoe);
+// console.log(animals[1], clones[1]);
+
+// class Animal{
+//     static belly =[]
+//     eat() {Animal.belly.push("food");}
+// }
+// let a =new Animal();
+// a.eat();
+// console.log(Animal.belly[0]);
+
+// var sound="grunt";
+// var bear ={sound:"roar"};
+// function roar(){
+//     console.log(this.sound)
+// }
+// roar.apply(bear)
+
+// let cat ={type:"tiger",size:"large"};
+// let json = JSON.stringify(cat,["type"]);
+// console.log(json)   //print{type:"tiger"}
+
+// const foo={
+//     bar(){
+//         console.log("hello, world");
+//     },
+//     name:"albert",
+//     age:26,
+// }
+// console.log(foo)
+
+// let rainForests= ["Amazon", "Borneo", "Cerrado","Congo"];
+// rainForests.splice(0,2);
+// console.log(rainForests);
+
+// let rainForests=10;
+// let animals=0;
+// while(rainForests<13 || animals<=2)
+// {
+//     rainForests++;
+//     animals+=2;
+// }
+// console.log(animals);
+
+// let animals=["eagle","osprey","salmon"];
+// let key =animal=>animal==="salmon";
+
+// if(animals.some(key))
+// {
+//     console.log("swim")
+// }
