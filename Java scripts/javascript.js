e806fafdcb23d1572c3b174e3952220373271858
Sourabh Shukla <sourabh01itsme@gmail.com>
2023-02-21 22:06:50 +0530

    add

--- Java scripts/javascript.js
+++ Java scripts/javascript.js
@@ -61,3 +61,36 @@
 // console.log(null || 2||3 || 4);
 
 // console.log(2&&3 &&0);
+
+// console.log(typeof NaN)
+// let nam="sourabh";
+// let age=26;
+// console.log(!typeof nam === "string")
+
+// You are given an integer array nums. You are initially positioned at the array's first index,
+// and each element in the array represents your maximum jump length at that position.
+// Return true if you can reach the last index, or false otherwise.
+
+// Example 1:
+
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+
+// Input: nums = [3,2,2,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximu
+
+let arr =['a','b','b','c','c','c'];
+const unique=(arr)=>
+{
+    let counter={};
+    arr.forEach((e)=>
+    {
+        counter[e]=(counter[e] || 0)+1;
+    })
+    return counter;
+}
+const res=unique(arr);
+console.log(res)
