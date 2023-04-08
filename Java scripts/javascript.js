125e3eadb8b14a9c00017e4696492b8dcce74453
Sourabh Shukla <sourabh01itsme@gmail.com>
2023-04-09 00:06:37 +0530

    added

--- Java scripts/javascript.js
+++ Java scripts/javascript.js
@@ -3,16 +3,10 @@
 
 // clones[0].type="bear";
 // clones[1]="sheep";
-// console.log(animals[0].type, clones[0].typoe);
+// console.log(animals[0].type, clones[0].type);
 // console.log(animals[1], clones[1]);
 
-// class Animal{
-//     static belly =[]
-//     eat() {Animal.belly.push("food");}
-// }
-// let a =new Animal();
-// a.eat();
-// console.log(Animal.belly[0]);
+//-----------------------------------------------------------
 
 // var sound="grunt";
 // var bear ={sound:"roar"};
@@ -21,51 +15,29 @@
 // }
 // roar.apply(bear)
 
-// let cat ={type:"tiger",size:"large"};
-// let json = JSON.stringify(cat,["type"]);
-// console.log(json)   //print{type:"tiger"}
-
-// const foo={
-//     bar(){
-//         console.log("hello, world");
-//     },
-//     name:"albert",
-//     age:26,
-// }
-// console.log(foo)
+//-----------------------------------------------------------
 
 // let rainForests= ["Amazon", "Borneo", "Cerrado","Congo"];
 // rainForests.splice(0,2);
 // console.log(rainForests);
 
-// let rainForests=10;
-// let animals=0;
-// while(rainForests<13 || animals<=2)
-// {
-//     rainForests++;
-//     animals+=2;
-// }
-// console.log(animals);
-
-// let animals=["eagle","osprey","salmon"];
-// let key =animal=>animal==="salmon";
-
-// if(animals.some(key))
-// {
-//     console.log("swim")
-// }
+//-----------------------------------------------------------
 
 // console.log(null || 4 || 2&&3 );
-//sourav = true;
-
 // console.log(null || 2||3 || 4);
-
-// console.log(2&&3 &&0);
-
+// console.log(2 && 3 && 0);
+// console.log(2 && 3 && 1 && 9);
+// console.log( 1 < 2 < 3);
+// console.log( 3 > 2 > 1);
+// console.log(0.2 + 0.3 == 0.5);
+// console.log(0.1 + 0.2 == 0.3);
 // console.log(typeof NaN)
 // let nam="sourabh";
-// let age=26;
 // console.log(!typeof nam === "string")
+
+//-----------------------------------------------------------
+
+
 
 // You are given an integer array nums. You are initially positioned at the array's first index,
 // and each element in the array represents your maximum jump length at that position.
